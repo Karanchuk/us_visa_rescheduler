127843d578d197c41ebb323cd1975558202d37cc
Ali-Flt <mr.ali1377@yahoo.com>
2023-12-27 02:54:29 +0000

    added file to create telegram session

---
+++ poll_telegram_channel/create_telegram_session.py
@@ -0,0 +1,14 @@
+from telethon.sync import TelegramClient
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', default='config.yaml')
+args = parser.parse_args()
+
+config = {}
+with open(args.config) as f:
+    config = yaml.load(f, Loader=yaml.loader.SafeLoader)
+
+TelegramClient(config['telegram']['session'], config['telegram']['api_id'], config['telegram']['api_hash']).start(phone=config['telegram']['phone_number'])
+print('.session file created successfully. DO NOT SHARE IT!')
